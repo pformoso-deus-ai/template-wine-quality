05218af78038e128bb460a3691f5f4e2ff80f9ec
Pablo Formoso <pablo.formoso@deus.ai>
2024-01-08 14:36:09 +0100

    Adding template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Example of a DS project with DVC, MLFlow and CDCI',
+    author='DEUS Data',
+    license='MIT',
+)
